5664cb4f11106363b3e3bbe399023af04b2df02f
Chris Kim <ckjr92@gmail.com>
2021-11-15 13:37:01 -0700

    finished lab

--- functions.test.js
+++ functions.test.js
@@ -1,7 +1,10 @@
 const {
     returnTwo,
     greeting,
-    add
+    add,
+    multiply,
+    divide,
+    subtract
 } = require('./functions.js')
 
 test ("returnTwo should return 2", () => {
@@ -16,9 +19,13 @@ describe("greeting function tests", () => {
     test("greeting with Jill as parameter", () => {
         expect(greeting('Jill')).toBe('Hello, Jill')
     })
+
+    test("greeting output should be string", () => {
+        expect(typeof greeting("Jill")).toBe('string')
+    })
 })
 
-describe("add function tests", () => {
+describe("Math functions", () => {
     test("add with 1 and 2", () => {
         expect(add(1,2)).toBe(3)
     })
@@ -26,5 +33,26 @@ describe("add function tests", () => {
     test("add with 5 and 9", () => {
         expect(add(5, 9)).toBe(14)
     })  
+
+    test("multiply with 5 and 9", () => {
+        expect(multiply(5, 9)).toBe(45)
+    })  
+
+    test("multiply with 5 and 9", () => {
+        expect(typeof multiply(5, 9)).not.toBe("boolean")
+    })  
+    
+    test("divide with 30 and 6", () => {
+        expect(divide(30, 6)).toBe(5)
+    })  
+
+    test("subtract with 8 and 1", () => {
+        expect(subtract(8, 1)).toBe(7)
+    })  
+
+    test("subtract with 8 and 1", () => {
+        expect(subtract(8, 1)).not.toContain('asdf')
+    })  
+
 })
 
